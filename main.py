42873ad0e0902bdb1cacbcabfb6663fdc7b1eb3a
Taras Faidiuk <taras.fid@gmail.com>
2024-04-19 20:26:38 +0300

    refactor: refactor codebase so we can get rid of app module

--- main.py
+++ main.py
@@ -1,34 +1,47 @@
-import flask
-from flask import Flask, request, jsonify, render_template, redirect
+from app import config
+from datetime import timedelta
+from flask import Flask, request, render_template, redirect
 from flask_mail import Mail, Message
-import sqlite3
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column
-from app.models import City
 
+# todo: refactor codebase so you can get rid of app module. (done)
+# todo: get rid of app module
 app = Flask(__name__)
-
-con = sqlite3.connect('instance/db.db')
-
-oon = con.cursor()
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
+app.config.from_object(config.Config)
+app.config['MAIL_SERVER'] = "sandbox.smtp.mailtrap.io" # todo: move to env or config file
+app.config['MAIL_PORT'] = "25" # todo: move to env or config file
+app.config['MAIL_USERNAME'] = "2b5c264c26ebc3" # todo: move to env or config file
+app.config['MAIL_PASSWORD'] = "eaa7a748843d9b" # todo: move to env or config file
+app.permanent_session_lifetime = timedelta(minutes=90)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+mail = Mail(app)
 
+
+class City(db.Model):
+    __tablename__ = 'city'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    description = Column(db.String)
+    img = Column(db.String)
 
 @app.route('/db_set')
 def show_template():
     if not City.query.all():
-        city = City(name='paris', description='good sity', img='http://127.0.0.1:5000/static/img/img-1.jpg')
-        city = City(name='new york', description='so good sity', img='http://127.0.0.1:5000/static/img/img-2.jpg')
-        city = City(name='london', description='so so good sity', img='http://127.0.0.1:5000/static/img/img-3.jpg')
-        city = City(name='amsterdam', description='SO SO SO GOOD SITY', img='http://127.0.0.1:5000/static/img/img-4.jpg')
-        city = City(name='australia', description='good sity', img='http://127.0.0.1:5000/static/img/img-5.jpg')
-        city = City(name='japan', description='good sity', img='http://127.0.0.1:5000/static/img/img-6.jpg')
-        db.session.add(city)
+        cities = []
+
+        cities.append(City(name='paris', description='good sity', img='http://127.0.0.1:5000/static/img/img-1.jpg'))
+        cities.append(City(name='new york', description='so good sity', img='http://127.0.0.1:5000/static/img/img-2.jpg'))
+        cities.append(City(name='london', description='so so good sity', img='http://127.0.0.1:5000/static/img/img-3.jpg'))
+        cities.append(City(name='amsterdam', description='SO SO SO GOOD SITY', img='http://127.0.0.1:5000/static/img/img-4.jpg'))
+        cities.append(City(name='australia', description='good sity', img='http://127.0.0.1:5000/static/img/img-5.jpg'))
+        cities.append(City(name='japan', description='good sity', img='http://127.0.0.1:5000/static/img/img-6.jpg'))
+        for city in cities:
+            db.session.add(city)
         db.session.commit()
+
         return 'cities was created'
 
     return 'cities exist'
@@ -38,14 +51,25 @@ def show_template():
 def main():
     return render_template('index.html')
 
+
 @app.route('/lst')
 def cont():
     return render_template('contact.html')
+
 
 @app.route('/listing')
 def listing():
     return render_template('listing.html')
 
+# todo: example
+# @app.route('/<city_name>')
+# def par(city_name):
+#     city = City.query.filter(name=city_name).first()
+#
+#     if not city:
+#         return str(f'City `{city_name}` Not Found'), 404
+#
+#     return render_template('paris.html', city=city)
 
 @app.route('/newyork')
 def new():
@@ -59,6 +83,7 @@ def par():
 def lon():
     return render_template('london.html')
 
+
 @app.route('/amst')
 def ams():
     return render_template('amst.html')
@@ -71,16 +96,17 @@ def ast():
 def jup():
     return render_template('jup.html')
 
+
 @app.route('/contactus', methods=['POST'])
 def contact_us():
     if request.method == 'POST':
         email = request.form['mail']
         name = request.form['name']
-        txt = request.form['textarea']
+        txt = request.form['textarea']  # todo: remove unused code
 
         msg = Message(
             subject='Contact letter',
-            sender='noreply@employer.com',
+            sender='noreply@employer.com',  # todo: set ur domain name (extra  todo: get domain using Flask)
             recipients=[email]
         )
         msg.html = f"""
@@ -99,21 +125,6 @@ def contact_us():
         mail.send(msg)
 
         return redirect('/lst')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
